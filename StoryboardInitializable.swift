b2a0259c1f396fb784175f5824359f01ce541cef
Jason <4234115@qq.com>
2019-12-18 15:49:40 +0800

    Create StoryboardInitializable.swift

---
+++ StoryboardInitializable.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+protocol StoryboardInitializable {
+    static var storyboardIdentifier: String { get }
+}
+
+extension StoryboardInitializable where Self: UIViewController {
+
+    static var storyboardIdentifier: String {
+        return String(describing: Self.self)
+    }
+
+    static func initFromStoryboard(name: String = "Main") -> Self {
+        let storyboard = UIStoryboard(name: name, bundle: Bundle.main)
+        return storyboard.instantiateViewController(withIdentifier: storyboardIdentifier) as! Self
+    }
+}
